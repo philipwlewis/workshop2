32cc33b4879ab831a7fff3f9fadc4d37266b237c
philipwlewis <philip.lewis@ucl.ac.uk>
2025-01-21 02:07:38 +0000

    reset

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,8 +1,3 @@
+// Add your code below
 
-// HelloWorld application
-public class HelloWorldApp {
-    public static void main(String[] args) {
-        System.out.println("Hello World! I am " + args[0]
-        + ". I am " + args[1] + " years old.");
-    } 
-}
+
